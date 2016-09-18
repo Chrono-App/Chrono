5b91b79cb47148a6dd4f83c60b9d5ed5158718eb
Rebecca Qian <RebeccaQian@Rebeccas-MacBook-Pro-2.local>
2016-09-18 02:05:05 -0700

    bla

--- webApp/Static/Views/events.js
+++ webApp/Static/Views/events.js
@@ -1,6 +1,20 @@
 var Chrono = Chrono || {};
 
 Chrono.eventsview = function() {
-	this._
-}
+		// create model
+		this.model = new Chrono.eventsmodel();
+		model.subscribe(this.onModelUpdate)
+		// subscribe event
+
+		// fetch!
+};
+
+Chrono.eventsview.prototype.render = function() {
+
+};
+
+Chrono.eventsview.prototype.onModelUpdate = function(eventType) {
+	// based on etype, do something!
+	console.log(this);
+};
 
